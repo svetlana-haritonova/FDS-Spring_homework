6fa8916674d19e797ddc7e2d4da2629a60a35957
svetlana <sveta.haritonova@icloud.com>
2025-04-09 21:49:22 +0300

    а

--- src/main/java/ru/tbank/fdsspring/model/Currency.java
+++ src/main/java/ru/tbank/fdsspring/model/Currency.java
@@ -1,16 +1,34 @@
 package ru.tbank.fdsspring.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Currency {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
     private String name;
-    private String baseCurrency;
+    private String baseCurrency = "RUB";
     private String priceChangeRange;
     private String description;
+
+    public Currency(String name, String baseCurrency, String priceChangeRange, String description) {
+        this.name = name;
+        this.baseCurrency = baseCurrency;
+        this.priceChangeRange = priceChangeRange;
+        this.description = description;
+    }
 }
--- src/main/java/ru/tbank/fdsspring/service/CurrencyService.java
+++ src/main/java/ru/tbank/fdsspring/service/CurrencyService.java
@@ -1,23 +1,68 @@
 package ru.tbank.fdsspring.service;
 
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.tbank.fdsspring.model.Currency;
+import ru.tbank.fdsspring.model.CurrencyRequest;
+import ru.tbank.fdsspring.repository.CurrencyRepository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-@Getter
+@RequiredArgsConstructor
 public class CurrencyService {
-    private final List<Currency> currencies = new ArrayList<>();
 
-    public void addCurrency(Currency currency) {
-        currencies.add(currency);
-        return;
+    private final CurrencyRepository currencyRepository;
+
+    @Transactional(readOnly = true)
+    public List<Currency> getCurrencies() {
+        return currencyRepository.findAllCurrencies().collect(Collectors.toList());
     }
 
-    public boolean deleteCurrency(String id) {
-        return currencies.removeIf(currency -> currency.getId().equals(id));
+    @Transactional
+    public void addCurrency(CurrencyRequest currencyRequest) {
+        Currency currency = new Currency();
+        currency.setName(currencyRequest.getName());
+        currency.setBaseCurrency(currencyRequest.getBaseCurrency());
+        currency.setPriceChangeRange(currencyRequest.getPriceChangeRange());
+        currency.setDescription(currencyRequest.getDescription());
+        currencyRepository.save(currency);
+    }
+
+    @Transactional
+    public Currency updateCurrency(Long id, CurrencyRequest currencyRequest) {
+        Currency updateCurrency = currencyRepository.findById(id).orElse(null);
+        if (updateCurrency != null) {
+            updateCurrency.setName(currencyRequest.getName());
+            updateCurrency.setBaseCurrency(currencyRequest.getBaseCurrency());
+            updateCurrency.setPriceChangeRange(currencyRequest.getPriceChangeRange());
+            updateCurrency.setDescription(currencyRequest.getDescription());
+            return updateCurrency;
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Currency getCurrencyById(Long id) {
+        return currencyRepository.findById(id).orElseThrow(() -> new RuntimeException((String) null));
+    }
+
+    public boolean isCurrencyEmpty(Currency currency) {
+        return (currency == null ||
+                currency.getId() == null ||
+                currency.getName() == null ||
+                currency.getBaseCurrency() == null ||
+                currency.getPriceChangeRange() == null ||
+                currency.getDescription() == null);
+    }
+
+
+    @Transactional
+    public void deleteCurrency(Long id) {
+        currencyRepository.deleteById(id);
     }
 }
